b912a3e50a24fbbd3083be7d58cab5210857fb00
Nikos Stylianou <stylianou404@gmail.com>
2023-11-07 19:39:30 +0200

    Created A1 and A2 matrices

--- Equation_Make/equation_make.c
+++ Equation_Make/equation_make.c
@@ -2,51 +2,107 @@
 
 int equation_make(Element *head, NodePair *pair_head, RetHelper helper) {
 	Element *current = NULL;
-	short int **A=NULL; // A[nodes_num][elements_num]
-	int elements_number=0;
-	int i=0;
+	short int **A1=NULL, **A2=NULL; // A1[nodes_num][m1] A1[nodes_num][m2]
+	int i=0, j=0;
 	int hash_p=-1, hash_n=-1;
-
-	// Create A matrix
-	A = calloc(helper.node_num, sizeof(short int *));
-	for (i=0;i<helper.node_num;i++) {
-		A[i] = calloc((helper.m1+helper.m2), sizeof(short int));
-	}
 
 	if (!head) {
 		print_error("equation_make", 4, "Element list head empty");
 	}
 
+	// Create A1 matrix
+	A1 = calloc(helper.node_num, sizeof(short int *));
+	if (!A1) {
+		print_error("equation_make", 3, "A1 array error!");
+	}
+	for (i=0;i<helper.node_num;i++) {
+		A1[i] = calloc((helper.m1), sizeof(short int));
+		if (!A1[i]) {
+			print_error("equation_make", 3, "Internal A1 array error!");
+		}
+	}
+
+	// Create A2 matrix
+	A2 = calloc(helper.node_num, sizeof(short int *));
+	if (!A2) {
+		print_error("equation_make", 3, "A2 array error!");
+	}
+	for (i=0;i<helper.node_num;i++) {
+		A2[i] = calloc((helper.m2), sizeof(short int));
+		if (!A2[i]) {
+			print_error("equation_make", 3, "Internal A2 array error!");
+		}
+	}
+
 	// Fill A matrix for debug (reduced incidence matrix)
-	for (current = head,i=0;current->next != NULL;current=current->next,i++) {
+	i=0;
+	j=0;
+	for (current = head;current->next != NULL;current=current->next) {
 
 		// Differentiation using m1 and m2
 //		if (current->type_of_element);
 
 		hash_p = find_node_pair(pair_head, current->node_p);
 		if (hash_p != 0) {
-			A[hash_p-1][i] = +1;
+//			A[hash_p-1][i] = +1;
+			switch (current->type_of_element) {
+				case 'r': {}
+				case 'c': {}
+				case 'i': {
+					A1[hash_p-1][i] = +1;
+					break;
+				}
+				case 'l':{}
+				case 'v':{
+					A2[hash_p-1][j] = +1;
+					break;
+				}
+			}
 		}
 
 		hash_n = find_node_pair(pair_head, current->node_n);
 		if (hash_n != 0) {
-			A[hash_n-1][i] = -1;
+			switch (current->type_of_element) {
+				case 'r': {}
+				case 'c': {}
+				case 'i': {
+					A1[hash_n-1][i++] = -1;
+					break;
+				}
+				case 'l':{}
+				case 'v':{
+					A2[hash_n-1][j++] = -1;
+					break;
+				}
+			}
 		}
 	}
 
-	// Print A matrix
+	// Print A1 matrix
 	for (i=0;i<helper.node_num;i++) {
-		for (int j=0;j<helper.m1+helper.m2;j++) {
-			printf("%3d ", A[i][j]);
+		for (int j=0;j<helper.m1;j++) {
+			printf("%3d ", A1[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+
+	// Print A2 matrix
+	for (i=0;i<helper.node_num;i++) {
+		for (int j=0;j<helper.m2;j++) {
+			printf("%3d ", A2[i][j]);
 		}
 		printf("\n");
 	}
 
 	// Free Mem
 	for (i=0;i<helper.node_num;i++) {
-		free(A[i]);
+		free(A1[i]);
+		free(A2[i]);
 	}
-	free(A);
+	free(A1);
+	free(A2);
 
 	return -1;
 }
--- main.c
+++ main.c
@@ -3,36 +3,36 @@
 #include "Equation_Make/equation_make.h"
 
 int main(int argc, char* argv[]) {
-    FILE *input_file = NULL;
-    Element *head = NULL;
-    NodePair *head_node_pair = NULL;
+	FILE *input_file = NULL;
+	Element *head = NULL;
+	NodePair *head_node_pair = NULL;
 	RetHelper ret = {0};
 
-    if (argc != 2) {
-        // no input file selected
-        print_error(argv[0], 1, NULL);
-    }
+	if (argc != 2) {
+		// no input file selected
+		print_error(argv[0], 1, NULL);
+	}
 
-    // Open file
-    input_file = fopen(argv[1], "rb");
-    if (input_file == NULL) {
-        print_error(argv[0], 2, NULL);
-    }
+	// Open file
+	input_file = fopen(argv[1], "rb");
+	if (input_file == NULL) {
+		print_error(argv[0], 2, NULL);
+	}
 
-    ret = parser(input_file, &head, &head_node_pair);
+	ret = parser(input_file, &head, &head_node_pair);
 
-	printf("List length: %d\n\n", get_list_length(head));
+//	printf("List length: %d\n\n", get_list_length(head));
 
-    print_list(head);
-    print_pairs(head_node_pair);
+//    print_list(head);
+//    print_pairs(head_node_pair);
 
-	printf("\nM1 elements: %d\nM2 elements: %d\n\n", ret.m1, ret.m2);
+//	printf("\nM1 elements: %d\nM2 elements: %d\n\n", ret.m1, ret.m2);
 
 	equation_make(head, head_node_pair, ret);
 
-    fclose(input_file);
-    fflush(stdout);
-    free_mem(NULL, head, head_node_pair);
+	fclose(input_file);
+	fflush(stdout);
+	free_mem(NULL, head, head_node_pair);
 
-    return 0;
+	return 0;
 }
