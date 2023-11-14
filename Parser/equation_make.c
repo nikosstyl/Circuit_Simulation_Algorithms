dbe6f29bbdca6588b8a44454e2c4d437c62e13e4
dimosthenisbouroutzikas <114659891+dimosthenisbouroutzikas@users.noreply.github.com>
2023-11-14 21:24:50 +0200

    Update equation_make.c

--- Parser/equation_make.c
+++ Parser/equation_make.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int create_matrix(NodePair *HashTable, Element *Element_list){
+int create_matrix(NodePair *HashTable, Element *Element_list, RetHelper *ret){
 
     Element *current = NULL;
 	double **A=NULL, *b = NULL; // A[nodes_num][elements_num]
+    unsigned long m2counter = 0;
 	int elements_number=0;
 	int i=0;
 	int hash_p=-1, hash_n=-1;
@@ -14,20 +15,20 @@ int create_matrix(NodePair *HashTable, Element *Element_list){
 		print_error("equation_make", 4, "Element list head empty");
 	}
 
-    b = calloc(amount_of_nodes-1+group2_size, sizeof(double));
+    b = calloc((ret->amount_of_nodes+ret->group2_size), sizeof(double));
     if(!b){
         printf("Something went wrong with stage 1 memory alloc. Exiting.\n");
         return -1;
     }
 
-    A = calloc((amount_of_nodes-1)+group2_size, sizeof(double));
+    A = calloc((ret->amount_of_nodes+ret->group2_size), sizeof(double));
     if(!A){
         free(b);
         printf("Something went wrong with stage 1 memory alloc. Exiting.\n");
         return -1;
     }
-    for(i=0; i < (amount_of_nodes-1+group2_size); i++){
-        A[i] = calloc(el_total_size,sizeof(double));
+    for(i=0; i < ((ret->amount_of_nodes+ret->group2_size)); i++){
+        A[i] = calloc((ret->amount_of_nodes+ret->group2_size),sizeof(double));
         if(!A[i]){
             printf("Something went wrong with stage 2 memory alloc. Exiting.\n");
             for(int j=0; j < i; j++){
@@ -38,76 +39,89 @@ int create_matrix(NodePair *HashTable, Element *Element_list){
             return -1;
         }
     }
-    for(i=0; i < amount_of_nodes-1+group2_size; i++){
+    for(i=0; i < (ret->amount_of_nodes+ret->group2_size); i++){
         b[i] = 0;
-        for(int j =0; j < el_total_size; j ++){
+        for(int j =0; j < (ret->amount_of_nodes+ret->group2_size); j ++){
             A[i][j] = 0;
         }
     }
-
+    printf("Check1, el_total_size: %lu amount_of_nodes: %lu group2: %lu\n", ret->el_total_size,ret->amount_of_nodes,ret->group2_size);
     current = Element_list;
-    for(i = 0; i < el_total_size; i++){
-        hash_p = find_node_pair(HashTable, current->node_p);
-		hash_n = find_node_pair(HashTable, current->node_n);
-        switch (Element_list->type_of_element)
+    for(current=Element_list;current->next!=NULL; current=current->next){
+        hash_p = (find_node_pair(HashTable, current->node_p));
+		hash_n = (find_node_pair(HashTable, current->node_n));
+        printf("hash_p: %d hash_n: %d currval %lf\n",hash_p, hash_n,current->value);
+        switch (current->type_of_element)
         {
-        case 'v':
-            if(strcmp(current->node_p,"0")!=0){
-                A[hash_p-1][amount_of_nodes-1+group2_size] = A[hash_p-1][amount_of_nodes-1+group2_size] + 1;
-                A[amount_of_nodes-1+group2_size][hash_p-1] = A[amount_of_nodes-1+group2_size][hash_p-1]+1;
+        case 'v':{
+            m2counter++;
+            if(hash_p!=0){
+                printf("hash_p-1: %d hash_n: %d\n 2nd index: %ld\n",hash_p, hash_n,ret->amount_of_nodes+ret->group2_size);
+                A[hash_p-1][ret->amount_of_nodes+m2counter-1] = A[hash_p-1][ret->amount_of_nodes+m2counter-1] + 1.0;
+                A[ret->amount_of_nodes+m2counter-1][hash_p-1] = A[ret->amount_of_nodes+m2counter-1][hash_p-1]+1.0;
+                printf("Done correctly\n");
                 b[hash_p-1] = b[hash_p-1]+current->value;
             }
-            if(strcmp(current->node_n,"0")!=0){
-                A[hash_n-1][amount_of_nodes-1+group2_size] = A[hash_n-1][amount_of_nodes-1+group2_size]-1;
-                A[amount_of_nodes-1+group2_size][hash_n-1] = A[amount_of_nodes-1+group2_size][hash_n-1]-1;
+            if(hash_n!=0){
+                A[hash_n-1][ret->amount_of_nodes+m2counter-1] = A[hash_n-1][ret->amount_of_nodes+m2counter-1]-1.0;
+                A[ret->amount_of_nodes+m2counter-1][hash_n-1] = A[ret->amount_of_nodes+m2counter-1][hash_n-1]-1.0;
                 b[hash_n-1] = b[hash_n-1]-current->value;
             }
             break;
-        case 'i':
-            if(strcmp(current->node_p,"0")!=0){
-                b[hash_p-1] = b[hash_p-1]+current->value;
+        }    
+        case 'i':{
+            if(hash_p!=0){
+                b[hash_p-1] = b[hash_p-1]-current->value;
             }
-            if(strcmp(current->node_n,"0")!=0){
-                b[hash_n-1] = b[hash_n-1]-current->value;
+            if(hash_n!=0){
+                b[hash_n-1] = b[hash_n-1]+current->value;
             }
             break;
-        case 'r':
-            if(strcmp(current->node_p,"0")!=0){
+        }    
+        case 'r':{
+            if(hash_p!=0){
                 A[hash_p-1][hash_p-1] = A[hash_p-1][hash_p-1] + (1/current->value);
-                if(strcmp(current->node_n,"0")!=0){
+                if(hash_n!=0){
                     A[hash_n-1][hash_p-1] = A[hash_n-1][hash_p-1]-(1/current->value);
                     A[hash_p-1][hash_n-1] = A[hash_p-1][hash_n-1] - (1/current->value);
                 }
             }
-            if(strcmp(current->node_n,"0")!=0){
+            if(hash_n!=0){
                 A[hash_n-1][hash_n-1] = A[hash_n-1][hash_n-1] + (1/current->value);
             }
             break;
-        case 'l':
-            if(strcmp(current->node_p,"0")!=0){
+        }    
+        case 'l':{
+            m2counter++;
+            if(hash_p!=0){
+                A[hash_p-1][ret->amount_of_nodes+m2counter-1] = A[hash_p-1][ret->amount_of_nodes+m2counter-1] + 1.0;
+                A[ret->amount_of_nodes+m2counter-1][hash_p-1] = A[ret->amount_of_nodes+m2counter-1][hash_p-1]+1.0;
                 b[hash_p-1] = b[hash_p-1]+0;
             }
-            if(strcmp(current->node_n,"0")!=0){
+            if(hash_n!=0){
+                A[hash_n-1][ret->amount_of_nodes+m2counter-1] = A[hash_n-1][ret->amount_of_nodes+m2counter-1]-1.0;
+                A[ret->amount_of_nodes+m2counter-1][hash_n-1] = A[ret->amount_of_nodes+m2counter-1][hash_n-1]-1.0;
                 b[hash_n-1] = b[hash_n-1]-0;
             }
             break;
+        }    
         case 'c':
             break;        
         default:
             break;
         }
-        current = current->next;
+        //current = current->next;
     }
 
     printf("A table is: \n");
-    for(i = 0; i < amount_of_nodes-1+group2_size; i++){
-        for(int j=0; j < amount_of_nodes-1+group2_size; j++){
+    for(i = 0; i < ret->amount_of_nodes+ret->group2_size; i++){
+        for(int j=0; j < ret->amount_of_nodes+ret->group2_size; j++){
             printf("%.5lf ",A[i][j]);
         }
         printf("\n");
     }
     printf("b table is: \n");
-    for(i = 0; i < amount_of_nodes-1+group2_size; i++){
+    for(i = 0; i < ret->amount_of_nodes+ret->group2_size; i++){
         printf("%.5lf ",b[i]);
     }
     return 0;
